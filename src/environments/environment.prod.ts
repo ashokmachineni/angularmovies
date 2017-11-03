68e280b62aac92a7b9d1d8cb2e986656dae4ac65
ashokmachineni <ashok5679m@gmail.com>
2017-11-03 01:28:40 -0400

    firebase deploy code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAcCeSkXCDqrWuIY1wQapAit8CxCrcP62k",
+    authDomain: "bookstore-fda7a.firebaseapp.com",
+    databaseURL: "https://bookstore-fda7a.firebaseio.com",
+    projectId: "bookstore-fda7a",
+    storageBucket: "bookstore-fda7a.appspot.com",
+    messagingSenderId: "1028956005427"
+  }
 };
